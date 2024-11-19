1151a14891ead8f45f39e8459068bcfbfe08d172
Andrea Funtò <d093154@xpia040.utenze.bankit.it>
2024-11-19 17:36:42 +0100

    Version with initial skeleton

--- main.go
+++ main.go
@@ -3,19 +3,74 @@ package main
 import (
 	"context"
 	"fmt"
-	"log/slog"
 	"os"
 	"os/signal"
 	"sync"
 	"syscall"
-	"time"
-
-	"math/rand"
 )
 
+func sender(ctx context.Context, wg *sync.WaitGroup, messages chan<- string) {
+	defer wg.Done()
+	// Producer
+	for i := 0; i < 1000; i++ {
+		messages <- fmt.Sprintf("Message %d", i)
+	}
+	close(messages)
+}
+
+func batcher(ctx context.Context, wg *sync.WaitGroup, messages <-chan string, batches chan<- []string) {
+	defer wg.Done()
+	// Batcher
+	batch := make([]string, 0, 50)
+	for msg := range messages {
+		batch = append(batch, msg)
+		if len(batch) == 50 {
+			batches <- batch
+			batch = make([]string, 0, 50)
+		}
+	}
+	if len(batch) > 0 {
+		batches <- batch
+	}
+	close(batches)
+}
+
+func receiver(ctx context.Context, wg *sync.WaitGroup, batches <-chan []string) {
+	defer wg.Done()
+	// Consumer
+	for batch := range batches {
+		fmt.Println(batch)
+	}
+}
+
+func main() {
+	// Create channels
+	messages := make(chan string)
+	batches := make(chan []string)
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	defer stop()
+
+	// Start goroutines
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	go sender(ctx, &wg, messages)
+
+	go batcher(ctx, &wg, messages, batches)
+
+	go receiver(ctx, &wg, batches)
+
+	wg.Wait()
+}
+
+/*
 func sendMessages(ctx context.Context, wg *sync.WaitGroup, in chan<- any) {
 	i := 0
-	defer wg.Done()
+	defer func() {
+		slog.Debug("sender done")
+		wg.Done()
+	}()
 loop:
 	for {
 		select {
@@ -26,12 +81,12 @@ loop:
 			// Send messages to the input channel
 			time.Sleep(time.Duration(int64(rand.Intn(10)) * int64(time.Millisecond)))
 			slog.Debug("enqueuing message", "message", i)
-			in <- i
-			i++
 			if ctx.Err() != nil {
 				slog.Debug("context cancelled")
 				break loop
 			}
+			in <- i
+			i++
 		}
 	}
 }
@@ -39,8 +94,11 @@ loop:
 func batchMessages(ctx context.Context, wg *sync.WaitGroup, in <-chan any, out chan<- []any, batchSize int, timeout time.Duration) {
 	var batch []any
 	ticker := time.NewTicker(timeout)
-	defer ticker.Stop()
-	defer wg.Done()
+	defer func() {
+		slog.Debug("batcher done")
+		ticker.Stop()
+		wg.Done()
+	}()
 
 loop:
 	for {
@@ -50,6 +108,10 @@ loop:
 			batch = append(batch, msg)
 			if len(batch) >= batchSize {
 				slog.Debug("sending out message batch", "batch", batch)
+				if ctx.Err() != nil {
+					slog.Debug("processing cancelled")
+					break loop
+				}
 				out <- batch
 				batch = nil
 			}
@@ -57,6 +119,10 @@ loop:
 			slog.Debug("timeout elapsed", "timeout", timeout)
 			if len(batch) > 0 {
 				slog.Debug("sending out messages batch", "batch", batch)
+				if ctx.Err() != nil {
+					slog.Debug("processing cancelled")
+					break loop
+				}
 				out <- batch
 				batch = nil
 			}
@@ -74,7 +140,11 @@ loop:
 }
 
 func receiveMessages(ctx context.Context, wg *sync.WaitGroup, out <-chan []any) {
-	defer wg.Done()
+	defer func() {
+		slog.Debug("receiver done")
+		wg.Done()
+	}()
+
 loop:
 	for {
 		select {
@@ -97,15 +167,19 @@ func main() {
 
 	var wg sync.WaitGroup
 
-	defer close(in)
+	defer func() {
+		slog.Debug("closing main")
+		close(in)
+		close(out)
+	}()
 
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	defer stop()
 
-	go batchMessages(ctx, &wg, in, out, 10, 50*time.Millisecond)
+	go sendMessages(ctx, &wg, in)
 	wg.Add(1)
 
-	go sendMessages(ctx, &wg, in)
+	go batchMessages(ctx, &wg, in, out, 10, 50*time.Millisecond)
 	wg.Add(1)
 
 	go receiveMessages(ctx, &wg, out)
@@ -115,10 +189,12 @@ func main() {
 	case <-time.After(5 * time.Second):
 		fmt.Println("missed signal")
 	case <-ctx.Done():
-		stop()
+		//stop()
 		fmt.Println("signal received")
 	}
+	slog.Debug("waiting for goroutines to complete")
 
 	wg.Wait()
 	//time.Sleep(20 * time.Millisecond)
 }
+*/
